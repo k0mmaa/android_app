19337f9368df63ab6dd12f34014c006e23ce2805
k0rs@_NB <kokosanet@gmail.com>
2024-05-27 16:45:57 +0300

    16:44

--- MyFirstApp/app/src/main/java/com/myfirstapp/MainActivity.kt
+++ MyFirstApp/app/src/main/java/com/myfirstapp/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.myfirstapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Spinner
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +12,16 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val findBeer = findViewById<Button>(R.id.find_beer) //create varible, findViewById<Button>(give the type of element).Give the ID
+        findBeer.setOnClickListener {
+            val beerColor = findViewById<Spinner>(R.id.beer_color)
+            val color = beerColor.selectedItem.toString()
+            val brands = findViewById<TextView>(R.id.brands)
+            brands.text = "Beer color is + $color"
+
+        }
         }
     }
-}
